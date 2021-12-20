c4c2dcc99ac987c35172ccb2f141a35eedf0aaf3
DennsHartstock <dennis.hartstock@web.de>
2021-12-20 07:58:17 +0100

    add volumeSeekBar

--- app/src/main/java/com/example/playingaudio/MainActivity.java
+++ app/src/main/java/com/example/playingaudio/MainActivity.java
@@ -2,20 +2,47 @@ package com.example.playingaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     Button button;
     boolean isPlaying = false;
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar = findViewById(R.id.seekBar);
+        volumeSeekBar.setMax(maxVolume);
+
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
 
